75dc9076430b99acc1b48821ffe4ad56b1507698
IamLite <tarie2@live.com>
2024-05-04 12:51:54 -0400

    initial commit

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Variables
 SCRIPT_NAME="wled_installer.sh"
 REPO_URL="https://raw.githubusercontent.com/iamlite/klipperwled/main"
-INSTALL_DIR="/usr/data/klipperwled/" # e.g., /usr/local/bin
+INSTALL_DIR="/usr/data/klipperwled" # e.g., /usr/local/bin
 
 # Create the installation directory if it doesn't exist
 mkdir -p "$INSTALL_DIR"
