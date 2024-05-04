a9160f0b519a7bb90e2d608476c81c9f29ed3c1a
IamLite <tarie2@live.com>
2024-05-04 12:50:22 -0400

    initial commit

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 # Variables
 SCRIPT_NAME="wled_installer.sh"
-REPO_URL="https://github.com/iamlite/klipperwled/blob/main"
+REPO_URL="https://raw.githubusercontent.com/iamlite/klipperwled/main"
 INSTALL_DIR="/usr/data/klipperwled/" # e.g., /usr/local/bin
 
 # Create the installation directory if it doesn't exist
 mkdir -p "$INSTALL_DIR"
 
 # Download the script
-curl -s "$REPO_URL/$SCRIPT_NAME?raw=true" -o "$INSTALL_DIR/$SCRIPT_NAME"
+curl -s "$REPO_URL/$SCRIPT_NAME" -o "$INSTALL_DIR/$SCRIPT_NAME"
 
 # Make the script executable
 chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
