c30427e55beafe026036de96fe990c1ce62cbf65
IamLite <tarie2@live.com>
2024-05-06 19:14:05 -0400

    .

--- Scripts/menu.sh
+++ Scripts/menu.sh
@@ -17,9 +17,7 @@ print_line() {
 show_menu() {
     clear
     print_line
-    printf ~~~~~~~~~~~~~~~~~~~~
     printf "${GREEN}%s\n" "WLED Klipper Setup Helper"
-    printf ~~~~~~~~~~~~~~~~~~~~
     print_line
     printf "${YELLOW}%s\n" "1. Setup WLED"
     printf "${YELLOW}%s\n" "2. Setup WLED Presets"
@@ -27,10 +25,7 @@ show_menu() {
     printf "${YELLOW}%s\n" "4. View and Edit Preset Numbers"
     printf "${YELLOW}%s\n" "5. Quit"
     print_line
-    print_line
-    printf ~~~~~~~~~~
     printf "${MAGENTA}Please enter your choice: ${NC}"
-    printf ~~~~~~~~~~
 }
 
 # Function to display a confirmation before proceeding
@@ -41,7 +36,7 @@ confirm_proceed() {
     printf "${GREEN}Proceed? (y/n): ${NC}"
     read yn
     if [ "$yn" = "y" ] || [ "$yn" = "Y" ]; then
-        sh "../$3"  # Adjust the path to go up one directory level then into the script
+        sh "$3"
     else
         printf "${RED}Action cancelled by the user.${NC}\n"
     fi
@@ -54,7 +49,7 @@ while true; do
     case "$choice" in
         1) confirm_proceed "setup WLED" "This will configure your WLED instance including IP address, LED count, and initial presets. It's essential for integrating WLED with your Klipper printer." "setup_wled.sh" ;;
         2) confirm_proceed "setup WLED Presets" "This will help you create and configure presets in your WLED setup for various printer events like pause, cancel, or resume." "setup_presets.sh" ;;
-        3) confirm_proceed "add Macros" "This option allows you to search and add specific macros to your printer's configuration, enabling more sophisticated control over your printer's behaviors." "macro_search.sh" ;;
+        3) confirm_proceed "add Macros" "This option allows you to search and add specific macros to your printer's configuration, enabling more sophisticated control over your printer's behaviors." "add_macros.sh" ;;
         4) confirm_proceed "view and edit preset numbers" "This allows you to view and modify the preset numbers assigned to different printer states, ensuring they match your current WLED setup." "view_edit_presets.sh" ;;
         5) printf "${BLUE}Exiting...${NC}\n"; break ;;
         *) printf "${RED}Invalid option, try again...${NC}\n" ;;
