042dd211bc94b471aea22f1b8a597caccaaaa97e
IamLite <tarie2@live.com>
2024-05-06 19:30:36 -0400

    .

--- start.sh
+++ start.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+# ASCII Art and Colors
+echo "\033[1;34m"
+echo "     __          __        _     _ _             _    _ _____ _     _ "
+echo "     \ \        / /       | |   | | |           | |  | |_   _| |   | |"
+echo "      \ \  /\  / /__  _ __| | __| | | ___  _   _| |  | | | | | |   | |"
+echo "       \ \/  \/ / _ \| '__| |/ _\` | |/ _ \| | | | |/\| | | | | |   | |"
+echo "        \  /\  / (_) | |  | | (_| | | (_) | |_| \  /\  /_| |_| |___| |___"
+echo "         \/  \/ \___/|_|  |_|\__,_|_|\___/ \__,_|\/  \/|_____|_____|_____|"
+echo "\033[0m"
+
 # Define the installation directory and repository URL
 INSTALL_DIR="/usr/data/WLED-Klipper-Helper"
 REPO_URL="https://github.com/iamlite/WLED-Klipper-Helper.git"
 
 # Ensure the script is run as root
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Error: This script must be run as root."
+    echo "\033[1;31mError: This script must be run as root.\033[0m"
     exit 1
 fi
 
@@ -15,17 +25,17 @@ set -ex
 
 # Clone the repository
 if [ -d "$INSTALL_DIR" ]; then
-    echo "The directory $INSTALL_DIR already exists. Removing..."
+    echo "\033[1;33mThe directory $INSTALL_DIR already exists. Removing...\033[0m"
     rm -rf "$INSTALL_DIR"
 fi
-echo "Cloning repository to $INSTALL_DIR..."
+echo "\033[1;32mCloning repository to $INSTALL_DIR...\033[0m"
 git clone "$REPO_URL" "$INSTALL_DIR"
-echo "Repository cloned."
+echo "\033[1;32mRepository cloned.\033[0m"
 
 # Set permissions for all scripts
-echo "Setting executable permissions on scripts..."
+echo "\033[1;34mSetting executable permissions on scripts...\033[0m"
 find "$INSTALL_DIR" -type f -name "*.sh" -exec chmod +x {} \;
 
 # Output instruction for starting the menu
-echo "Setup complete. To run the main menu, execute:"
-echo "$INSTALL_DIR/Scripts/Menu/menu.sh"
+echo "\033[1;36mSetup complete. To run the main menu, execute:\033[0m"
+echo "\033[1;35m$INSTALL_DIR/Scripts/Menu/menu.sh\033[0m"
