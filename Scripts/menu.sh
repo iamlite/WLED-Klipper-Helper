237e6ffbd5c29d60e572c8dcefe8ef53527e791a
IamLite <tarie2@live.com>
2024-05-06 21:05:29 -0400

    .

--- Scripts/menu.sh
+++ Scripts/menu.sh
@@ -5,6 +5,8 @@ CYAN='\033[0;36m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 MAGENTA='\033[1;35m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 # Function to print an ASCII line border
@@ -31,38 +33,42 @@ print_centered() {
 show_menu() {
     clear
     print_line
-    print_spacer # Blank line for spacing
+    print_spacer
     print_centered "WLED Klipper Setup Helper" "$GREEN"
-    print_spacer # Blank line for spacing
+    print_spacer
     print_line
-    print_spacer # Blank line for spacing
+    print_spacer
     print_centered "1. Setup WLED" "$YELLOW"
-    print_spacer # Blank line for spacing
+    print_spacer
     print_centered "2. Setup WLED Presets" "$YELLOW"
-    print_spacer # Blank line for spacing
+    print_spacer
     print_centered "3. Add Macros" "$YELLOW"
-    print_spacer # Blank line for spacing
+    print_spacer
     print_centered "4. View and Edit Preset Numbers" "$YELLOW"
-    print_spacer # Blank line for spacing
+    print_spacer
     print_centered "5. Quit" "$YELLOW"
-    print_spacer # Blank line for spacing
+    print_spacer
     print_line
     echo -e "${MAGENTA}Please enter your choice: ${NC}"
 }
 
-# Show the menu
-show_menu
-irmation before proceeding
+# Confirmation before proceeding
 confirm_proceed() {
-    printf "${BLUE}You chose to $1. Here's what this will do:\n${NC}"
-    printf "${MAGENTA}$2\n${NC}"
+    clear
     print_line
-    printf "${GREEN}Proceed? (y/n): ${NC}"
+    print_spacer
+    print_centered "You chose to $1" "$BLUE"
+    print_spacer
+    print_centered "$2" "$MAGENTA"
+    print_spacer
+    print_line
+    echo -e "${GREEN}Proceed? (y/n): ${NC}"
     read yn
     if [ "$yn" = "y" ] || [ "$yn" = "Y" ]; then
         sh "${SCRIPT_DIR}/$3"  # Execute the script using its absolute path
     else
-        printf "${RED}Action cancelled by the user.${NC}\n"
+        echo -e "${RED}Action cancelled by the user.${NC}"
+        print_spacer
     fi
 }
 
@@ -75,9 +81,9 @@ while true; do
         2) confirm_proceed "setup WLED Presets" "This will help you create and configure presets in your WLED setup for various printer events like pause, cancel, or resume." "setup_presets.sh" ;;
         3) confirm_proceed "add Macros" "This option allows you to search and add specific macros to your printer's configuration, enabling more sophisticated control over your printer's behaviors." "add_macros.sh" ;;
         4) confirm_proceed "view and edit preset numbers" "This allows you to view and modify the preset numbers assigned to different printer states, ensuring they match your current WLED setup." "view_edit_presets.sh" ;;
-        5) printf "${BLUE}Exiting...${NC}\n"; break ;;
-        *) printf "${RED}Invalid option, try again...${NC}\n" ;;
+        5) echo -e "${BLUE}Exiting...${NC}"; print_spacer; break ;;
+        *) echo -e "${RED}Invalid option, try again...${NC}"; print_spacer ;;
     esac
-    printf "${BLUE}Press enter to continue...${NC}"
+    echo -e "${BLUE}Press enter to continue...${NC}"
     read dummy
 done
