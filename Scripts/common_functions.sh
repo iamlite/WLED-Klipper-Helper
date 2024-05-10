c060a8594c13e9b6425669cb92baa47b4d70302c
IamLite <tarie2@live.com>
2024-05-10 02:15:54 -0400

    .

--- Scripts/common_functions.sh
+++ Scripts/common_functions.sh
@@ -78,7 +78,7 @@ BG_IMAGENTA='\033[0;105m'
 BG_ICYAN='\033[0;106m'
 BG_IWHITE='\033[0;107m'
 
-RAINBOW_COLORS=('\033[0;31m' '\033[0;33m' '\033[0;32m' '\033[0;36m' '\033[0;34m' '\033[0;35m')
+# RAINBOW_COLORS=('\033[0;31m' '\033[0;33m' '\033[0;32m' '\033[0;36m' '\033[0;34m' '\033[0;35m')
 
 # No Color (to reset to default)
 NC='\033[0m'
