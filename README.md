20d7d234a38c1aa100392b300d08aca718e48e12
IamLite <tarie2@live.com>
2024-05-10 11:36:46 -0400

    .

--- README.md
+++ README.md
@@ -5,11 +5,13 @@ Status: Not working - do not use (yet)
 A helper script for installing and configuring WLED with Klipper.
 This script will automatically add triggers to your macros to control WLED's behavior based on your printers status.
 
-curl -sSL "https://raw.githubusercontent.com/iamlite/WLED-Klipper-Helper/main/start.sh?$(date +%s)" | sudo sh
+## For Ender 3 V3 KE, Ender 3V3 and Creality K1, as well as anything using the nebula pad
 
-curl -sSL "https://raw.githubusercontent.com/iamlite/WLED-Klipper-Helper/main/start.sh?$(date +%s)" | INSTALL_DIR=/usr/data2/WLED-Klipper-Helper sudo sh
+curl -sSL "<https://raw.githubusercontent.com/iamlite/WLED-Klipper-Helper/main/start.sh?$(date> +%s)" | sudo sh
 
+## For any other klipper installs. Make sure to edit the INSTALL_DIR variable to choose where it is installed
 
+curl -sSL "<https://raw.githubusercontent.com/iamlite/WLED-Klipper-Helper/main/start.sh?$(date> +%s)" | sudo INSTALL_DIR=/YOUR/CUSTOM/PATH/WLED-Klipper-Helper sh
 
 ```bash
   ├─ Config/
--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ fi
 # Define default installation directory and repository URL
 DEFAULT_INSTALL_DIR="/usr/data/WLED-Klipper-Helper"
 REPO_URL="https://github.com/iamlite/WLED-Klipper-Helper.git"
-CONFIG_FILE="Config/settings.conf"  # Changed the path to save inside Config folder
+CONFIG_FILE="Config/settings.conf" 
 
 # Ensure the script is run as root
 if [ "$(id -u)" -ne 0 ]; then
@@ -24,14 +24,25 @@ fi
 
 # Use environment variable or default for the installation directory
 INSTALL_DIR="${INSTALL_DIR:-$DEFAULT_INSTALL_DIR}"
+CONFIG_DIR="$(dirname "$INSTALL_DIR/$CONFIG_FILE")"
 
 printf "\033[0;33m%s\033[0m\n" "Installation directory is set to $INSTALL_DIR"
 
 # Ensure the configuration directory exists
-mkdir -p "$(dirname "$INSTALL_DIR/$CONFIG_FILE")"
+if mkdir -p "$CONFIG_DIR"; then
+    printf "\033[0;32m%s\033[0m\n" "Configuration directory created at $CONFIG_DIR."
+else
+    printf "\033[0;31m%s\033[0m\n" "Failed to create configuration directory."
+    exit 1
+fi
 
 # Write the installation directory to a configuration file
-echo "INSTALL_DIR=$INSTALL_DIR" > "$INSTALL_DIR/$CONFIG_FILE"
+if echo "INSTALL_DIR=$INSTALL_DIR" > "$INSTALL_DIR/$CONFIG_FILE"; then
+    printf "\033[0;32m%s\033[0m\n" "Installation directory saved in settings file."
+else
+    printf "\033[0;31m%s\033[0m\n" "Failed to write installation directory to settings file."
+    exit 1
+fi
 
 # Check if the target directory exists and clear it
 if [ -d "$INSTALL_DIR" ]; then
@@ -52,4 +63,4 @@ find "$INSTALL_DIR" -type f -name "*.sh" -exec chmod +x {} \;
 
 # Output instruction for starting the menu
 printf "\033[0;36m%s\033[0m\n" "Setup complete. To run the main menu, execute:"
-printf "\033[0;35m%s\033[0m\n" "$INSTALL_DIR/Scripts/menu.sh"
+printf "\033[0;35m%s\033[0m\n" "$INSTALL_ALLOC_DIR/Scripts/menu.sh"
